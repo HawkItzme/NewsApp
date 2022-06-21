3eae8559ff7cff8073336bb1a7dc14696315f8c7
rhymezxcode <rhymezx.code@gmail.com>
2022-06-21 06:17:02 +0100

    Bug fixes, check the TODO. You can reach out, if you need more help, Goodluck bro

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -5,10 +5,10 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.android.volley.AuthFailureError
-import com.android.volley.Header
-import com.android.volley.Request
-import com.android.volley.Response
+import com.android.volley.*
+import com.android.volley.toolbox.BasicNetwork
+import com.android.volley.toolbox.DiskBasedCache
+import com.android.volley.toolbox.HurlStack
 import com.android.volley.toolbox.JsonObjectRequest
 import com.example.newsapp.databinding.ActivityMainBinding
 import org.json.JSONObject
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity(), NewsItemsClicked {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        mAdapter= NewsListAdapter( this)
+        mAdapter = NewsListAdapter(this)
 
         binding.recylerView.layoutManager = LinearLayoutManager(this)
         binding.recylerView.adapter = mAdapter
@@ -31,40 +31,49 @@ class MainActivity : AppCompatActivity(), NewsItemsClicked {
     }
 
     private fun fetchData() {
-        val url = "https://newsapi.org/v2/top-headlines?country=in&category=business"
-        val jsonObjectRequest :JsonObjectRequest = object : JsonObjectRequest(
-            Method.GET, url, null,
-            Response.Listener {
-                val  newsJsonArray = it.getJSONArray("articles")
-                val newsArray = ArrayList<News>()
-                for(i in 0 until newsJsonArray.length()){
-                    val newsJsonObject = newsJsonArray.getJSONObject(i)
-                    val news = News(
-                        newsJsonObject.getString("title"),
-                        newsJsonObject.getString("author"),
-                        newsJsonObject.getString("url"),
-                        newsJsonObject.getString("urltoImage"),
-                    )
-                    newsArray.add(news)
+        //TODO: To avoid crash, i added a try and catch block, just check the logcat for issues without the app crashing.
+        try {
+            val url = "https://newsapi.org/v2/top-headlines?country=in&category=business"
+            val jsonObjectRequest: JsonObjectRequest = object : JsonObjectRequest(
+                Method.GET, url, null,
+                Response.Listener {
+                    Log.e("FetchData: ", it.toString())
+                    val newsJsonArray = it.getJSONArray("articles")
+                    val newsArray = ArrayList<News>()
+                    for (i in 0 until newsJsonArray.length()) {
+                        val newsJsonObject = newsJsonArray.getJSONObject(i)
+                        //TODO: the app crashed because of "urltoImage", it was "urlToImage" in the jsonObject in the jsonArray.
+                        val news = News(
+                            newsJsonObject.getString("title"),
+                            newsJsonObject.getString("author"),
+                            newsJsonObject.getString("url"),
+                            newsJsonObject.getString("urlToImage"),
+                        )
+                        newsArray.add(news)
+                    }
+                    mAdapter.updateNews(newsArray)
+                },
+                Response.ErrorListener {
+                    it.message?.let { error -> Log.e("FetchDataError: ", error) }
+                }) {
+                @Throws(AuthFailureError::class)
+                override fun getHeaders(): Map<String, String> {
+                    val headers = HashMap<String, String>()
+                    //TODO: Added all the necessary headers your volley connection needs.
+                    //TODO: headers["User-Agent"] = "Mozilla/5.0", was the source for the error
+                    headers["Content-Type"] = "application/json"
+                    headers["User-Agent"] = "Mozilla/5.0"
+                    headers["x-api-key"] = "499653c0a19a4a2d86617ce635b1c417"
+                    return headers
                 }
-                mAdapter.updateNews(newsArray)
-
-                Log.e("FetchData: ", it.toString())
-            },
-            Response.ErrorListener{
-                it.message?.let { error -> Log.e("FetchDataError: ", error) }
-            }) {
-            @Throws(AuthFailureError::class)
-            override fun getHeaders(): Map<String, String> {
-                val headers = HashMap<String, String>()
-                headers["Content-Type"] = "application/json"
-                headers["x-api-key"] = "499653c0a19a4a2d86617ce635b1c417"
-                return headers
             }
+//         Access the RequestQueue through your singleton class.
+            MySingleton.getInstance(this)
+                .addToRequestQueue(jsonObjectRequest)
+        }catch(e: Exception){
+            e.printStackTrace()
         }
-        // Access the RequestQueue through your singleton class.
-        MySingleton.getInstance(this)
-            .addToRequestQueue(jsonObjectRequest)
+
 
 
     }
